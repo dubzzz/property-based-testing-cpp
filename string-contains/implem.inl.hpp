a6c6f2d34c6b44cb9299a4798e74d70eaad23d5f
Nicolas DUBIEN <github@dubien.org>
2017-06-28 20:55:48 +0200

    Initial build

---
+++ string-contains/implem.inl.hpp
@@ -0,0 +1,7 @@
+#include <string>
+
+bool contains(std::string const& pattern, std::string const& text)
+{
+  return false;
+}
+
---
+++ string-contains/main.cpp
@@ -0,0 +1,38 @@
+#include <gtest/gtest.h>
+#include <rapidcheck/gtest.h>
+
+#include <string>
+#include "implem.inl.hpp"
+
+TEST(Contains, StartsWith)
+{
+  ASSERT_TRUE(contains("he", "hello world"));
+}
+TEST(Contains, SomewhereInside)
+{
+  ASSERT_TRUE(contains("g", "omg!!!"));
+}
+TEST(Contains, ContainsEmptyString)
+{
+  ASSERT_TRUE(contains("", "whatever"));
+}
+TEST(Contains, NoMatch)
+{
+  ASSERT_FALSE(contains("no", "other"));
+}
+TEST(Contains, LargerPattern)
+{
+  ASSERT_FALSE(contains("no", ""));
+}
+
+RC_GTEST_PROP(Contains_Property, BAlwaysInABC, (std::string a, std::string b, std::string c))
+{
+  RC_ASSERT(contains(b, a + b + c));
+}
+
+int main(int argc, char **argv)
+{
+  ::testing::InitGoogleTest(&argc, argv);
+  int ret { RUN_ALL_TESTS() };
+  return ret;
+}
