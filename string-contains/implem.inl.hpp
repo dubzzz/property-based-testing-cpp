fd233048ae94d8903b41921a72dd2c5f94b18a3f
Nicolas DUBIEN <github@dubien.org>
2017-06-28 21:08:55 +0200

    [string-contains] implementation #1

--- string-contains/implem.inl.hpp
+++ string-contains/implem.inl.hpp
@@ -1,7 +1,19 @@
 #include <string>
 
+bool starts_with(const char* pattern, const char* text)
+{
+   for ( ; *pattern == *text ; ++pattern, ++text);
+   return !*pattern;
+}
+
+bool contains(const char* pattern, const char* text)
+{
+   while (*text && ! starts_with(pattern, text++));
+   return !!*text;
+}
+
 bool contains(std::string const& pattern, std::string const& text)
 {
-  return false;
+  return contains(pattern.c_str(), text.c_str());
 }
 
