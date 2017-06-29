8aa06ba6251f12ba231f3b6ecd9ea233c8e97d12
Nicolas DUBIEN <github@dubien.org>
2017-06-29 08:09:53 +0200

    [string-contains] implementation #5
    missing some boundaries checks

--- string-contains/implem.5.inl.hpp
+++ string-contains/implem.5.inl.hpp
@@ -3,15 +3,15 @@
 bool starts_with(const char* pattern, const char* pattern_end, const char* text, const char* text_end)
 {
   for ( ; pattern != pattern_end && *pattern == *text ; ++pattern, ++text);
-  return !*pattern;
+  return pattern == pattern_end;
 }
 
 bool contains(const char* pattern, const char* pattern_end, const char* text, const char* text_end)
 {
-  if (!*pattern) return true;
+  if (pattern == pattern_end) return true;
 
   for ( ; text != text_end && ! starts_with(pattern, pattern_end, text, text_end) ; ++text);
-  return !!*text;
+  return text != text_end;
 }
 
 bool contains(std::string const& pattern, std::string const& text)
