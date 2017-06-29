ec50a56c4e4b4d50eccea501bbc2667aceafd08c
Nicolas DUBIEN <github@dubien.org>
2017-06-29 08:41:23 +0200

    [string-contains] add properties

--- string-contains/main.cpp
+++ string-contains/main.cpp
@@ -1,6 +1,7 @@
 #include <gtest/gtest.h>
 #include <rapidcheck/gtest.h>
 
+#include <cstddef>
 #include <string>
 #include "implem.inl.hpp"
 
@@ -34,6 +35,11 @@ TEST(ContainsBug, Implem2)
   ASSERT_TRUE(contains("a", "a"));
 }*/
 
+RC_GTEST_PROP(Contains_Property, NoNullBAlwaysInABC, (std::string a, std::string b, std::string c))
+{
+  RC_ASSERT(contains(b, a + b + c));
+}
+
 RC_GTEST_PROP(Contains_Property, BAlwaysInABC, ())
 {
   auto gen = rc::gen::container<std::string>(rc::gen::arbitrary<char>());
@@ -41,6 +47,27 @@ RC_GTEST_PROP(Contains_Property, BAlwaysInABC, ())
   std::string b = *gen;
   std::string c = *gen;
   RC_ASSERT(contains(b, a + b + c));
+}
+
+bool property_if_only_if(std::string const& pattern, std::string const& text)
+{
+  for (std::size_t idx {} ; idx != text.size() ; ++idx)
+    if (text.substr(idx, pattern.size()) == pattern)
+      return true;
+  return pattern.empty();
+}
+
+RC_GTEST_PROP(Contains_Property, NoNullIfAndOnlyIfIndexSuchThatSubstringIsPattern, (std::string pattern, std::string text))
+{
+  RC_ASSERT(property_if_only_if(pattern, text) == contains(pattern, text));
+}
+
+RC_GTEST_PROP(Contains_Property, IfAndOnlyIfIndexSuchThatSubstringIsPattern, ())
+{
+  auto gen = rc::gen::container<std::string>(rc::gen::arbitrary<char>());
+  std::string pattern = *gen;
+  std::string text = *gen;
+  RC_ASSERT(property_if_only_if(pattern, text) == contains(pattern, text));
 }
 
 int main(int argc, char **argv)
