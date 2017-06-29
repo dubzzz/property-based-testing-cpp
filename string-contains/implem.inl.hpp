c3c795a9c1df6c2e29b3beaa072fc698b017b1d0
Nicolas DUBIEN <github@dubien.org>
2017-06-29 08:44:49 +0200

    [string-contains] implementation #6
    fix does not contain test-cases

--- string-contains/implem.inl.hpp
+++ string-contains/implem.inl.hpp
@@ -2,14 +2,14 @@
 
 bool starts_with(const char* pattern, const char* pattern_end, const char* text, const char* text_end)
 {
-  for ( ; pattern != pattern_end && *pattern == *text ; ++pattern, ++text);
+  for ( ; pattern != pattern_end && text != text_end && *pattern == *text ; ++pattern, ++text);
   return pattern == pattern_end;
 }
 
 bool contains(const char* pattern, const char* pattern_end, const char* text, const char* text_end)
 {
   if (pattern == pattern_end) return true;
-
+  
   for ( ; text != text_end && ! starts_with(pattern, pattern_end, text, text_end) ; ++text);
   return text != text_end;
 }
