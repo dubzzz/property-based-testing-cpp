4fb82061af9883eb3d9329dcce455fcd5f4cec6f
Nicolas DUBIEN <dubzzz@users.noreply.github.com>
2017-07-06 11:28:16 +0200

    Update implem.2bis.inl.hpp

--- shadows-of-the-knight/implem.2bis.inl.hpp
+++ shadows-of-the-knight/implem.2bis.inl.hpp
@@ -18,23 +18,23 @@ void locate_in_space(Space& space, std::size_t rounds)
     if (hint[0] == 'U')
     {
       y_max = y0;
-      y0 = (y_max + y_min) /2;
+      y0 = (y_max + y_min +1) /2;
     }
     else if (hint[0] == 'D')
     {
       y_min = y0 +1;
-      y0 = (y_max + y_min) /2;
+      y0 = (y_max + y_min +1) /2;
     }
     
     if (hint.back() == 'L')
     {
       x_max = x0;
-      x0 = (x_max + x_min) /2;
+      x0 = (x_max + x_min +1) /2;
     }
     else if (hint.back() == 'R')
     {
       x_min = x0 +1;
-      x0 = (x_max + x_min) /2;
+      x0 = (x_max + x_min +1) /2;
     }
     
     space.move(x0, y0);
