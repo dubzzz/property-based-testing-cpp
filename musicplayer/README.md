ccf9f95b48d862567ec588950c13f23e5ee99885
Nicolas DUBIEN <github@dubien.org>
2017-08-11 20:44:47 +0200

    [musicplayer] basic implementation and unit-tests

--- musicplayer/README.md
+++ musicplayer/README.md
@@ -10,8 +10,7 @@ Lets assume we are building a music player having the following characteristics:
 - Pause: pauses the currently playing music
 - Next: moves to next track (regarding ordering scheme in place), does not impact playing mode status, starts the music at its beginning
 - Add Track At Position: adds a track at a given position into the playlist (basically we can imagine some kind of drag-and-drop)
-- Toggle Repeat: activate or desactivate repeat mode of the playlist
 
 For the sake of this example, we will simplify the notion of track and position in the track:
 - track will be considered as a simple label
-- position in the track will not be taken into account (lets add Notify Track End to be able to emulate the end of the track)
+- position in the track will not be taken into account (as a consequence moving to next track results in a call to Next)
--- musicplayer/main.cpp
+++ musicplayer/main.cpp
@@ -1,7 +1,90 @@
 #include <gtest/gtest.h>
 #include <rapidcheck/gtest.h>
 
+#include <vector>
+
 #include "implem.inl.hpp"
+
+// Running tests
+
+TEST(MusicPlayer, StartInPauseModeOnFirstTrack)
+{
+	MusicPlayer p{ "track:aaa", "track:bbb" };
+	ASSERT_FALSE(p.is_playing());
+	ASSERT_EQ("track:aaa", p.track_name());
+}
+
+TEST(MusicPlayer, PlayOnlyChangePlayingStatusNotTheTrack)
+{
+	MusicPlayer p{ "track:aaa", "track:bbb" };
+	p.play();
+	p.play();
+	ASSERT_TRUE(p.is_playing());
+	ASSERT_EQ("track:aaa", p.track_name());
+}
+TEST(MusicPlayer, MultiplePlaySameAsSinglePlay)
+{
+	MusicPlayer p{ "track:aaa", "track:bbb" };
+	p.play();
+	p.play();
+	ASSERT_TRUE(p.is_playing());
+	ASSERT_EQ("track:aaa", p.track_name());
+}
+
+TEST(MusicPlayer, PauseOnlyChangePlayingStatusNotTheTrack)
+{
+	MusicPlayer p{ "track:aaa", "track:bbb" };
+	p.play();
+	p.pause();
+	ASSERT_FALSE(p.is_playing());
+	ASSERT_EQ("track:aaa", p.track_name());
+}
+
+TEST(MusicPlayer, NextDoesNotChangePlayingStatus)
+{
+	MusicPlayer p{ "track:aaa", "track:bbb", "track:ccc" };
+	p.next();
+	ASSERT_FALSE(p.is_playing());
+	p.play();
+	p.next();
+	ASSERT_TRUE(p.is_playing());
+}
+
+TEST(MusicPlayer, NextDoesCircularTraversalOfTracks)
+{
+	MusicPlayer p{ "track:aaa", "track:bbb", "track:ccc" };
+	p.next();
+	ASSERT_EQ("track:bbb", p.track_name());
+	p.next();
+	ASSERT_EQ("track:ccc", p.track_name());
+	p.next();
+	ASSERT_EQ("track:aaa", p.track_name());
+}
+
+TEST(MusicPlayer, AddedTrackAreInstantlyAvailable)
+{
+	MusicPlayer p{ "track:aaa", "track:bbb", "track:ccc" };
+	p.add_track("track:ddd", 1);
+	p.add_track("track:eee", 0);
+	p.add_track("track:fff", 5);
+
+	p.jump(0);
+	ASSERT_EQ("track:eee", p.track_name());
+	p.jump(2);
+	ASSERT_EQ("track:ddd", p.track_name());
+	p.jump(5);
+	ASSERT_EQ("track:fff", p.track_name());
+}
+
+TEST(MusicPlayer, AddTrackDoesNotImplyPlayingStatusOrTrack)
+{
+	MusicPlayer p{ "track:aaa", "track:bbb", "track:ccc" };
+	p.play();
+	p.next();
+	p.add_track("track:ddd", 2);
+	ASSERT_TRUE(p.is_playing());
+	ASSERT_EQ("track:bbb", p.track_name());
+}
 
 int main(int argc, char **argv)
 {
